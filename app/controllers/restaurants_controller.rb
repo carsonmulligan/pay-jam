034065224c143f43c2525d4bc66686c28e03e7b1
carson mulligan <carsonmulligan45@gmail.com>
2022-05-31 07:29:52 +0100

    restaurants/new view page created

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,13 @@
 class RestaurantsController < ApplicationController
   def new
-    @restaurant = Restaurant.new
+    @restaurant = Restaurant.new(restaurant_params)
     @user = User.find(params[:id])
     @restaurant.save!
   end
+end
+
+private
+
+def restaurant_params
+  @restaurant.params(:name, :address).permit(:name, :address)
 end
