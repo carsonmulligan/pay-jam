03d4ef59f69738b8f91a1f85f08cd00aa88aecc6
carson mulligan <carsonmulligan45@gmail.com>
2022-05-31 09:58:08 +0100

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  get 'tab_dishes/index'
-  get 'tab_dishes/show'
-  get 'tab_dishes/new'
-  get 'tab_dishes/create'
-  get 'tab_dishes/edit'
-  get 'tab_dishes/update'
-  get 'tab_dishes/destroy'
   devise_for :users
   root to: 'pages#home'
 
   resources :restaurants
   resources :dishes
   resources :tabs, only: [:index, :show]
+  resources :tab_dishes
   resources :bill_dishes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
