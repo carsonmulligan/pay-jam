535525f1d069573c09225d2b741d2628517bf5d4
carson mulligan <carsonmulligan45@gmail.com>
2022-05-31 07:56:47 +0100

    restaurants controller updated

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,62 @@
 class RestaurantsController < ApplicationController
-  def new
-    @restaurant = Restaurant.new(restaurant_params)
-    @user = User.find(params[:id])
-    @restaurant.save!
+
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy, :publish]
+
+  # GET /restaurants/1
+  def show
   end
-end
 
-private
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+    authorize @restaurant
+  end
 
-def restaurant_params
-  @restaurant.params(:name, :address).permit(:name, :address)
+  # GET /restaurants/1/edit
+  def edit
+  end
+
+  # POST /restaurants
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    authorize @restaurant
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /restaurants/1
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant, notice: 'Restaurant was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /restaurants/1
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_url, notice: 'Restaurant was successfully destroyed.'
+  end
+
+  def publish
+    @restaurant.status = 'published'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+      authorize @restaurant
+    end
+
+    # Only allow a list of trusted parameters through.
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :user_id)
+    end
 end
