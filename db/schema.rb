d6b33fc2d47c41944146cf4ca74df851cd43d0c5
carson mulligan <carsonmulligan45@gmail.com>
2022-06-01 12:14:45 +0100

    migrated bills

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_31_144305) do
+ActiveRecord::Schema.define(version: 2022_06_01_111404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bills", force: :cascade do |t|
+    t.decimal "total"
+    t.integer "status"
+    t.bigint "tab_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["tab_id"], name: "index_bills_on_tab_id"
+  end
 
   create_table "dishes", force: :cascade do |t|
     t.string "name"
@@ -66,6 +75,7 @@ ActiveRecord::Schema.define(version: 2022_05_31_144305) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bills", "tabs"
   add_foreign_key "dishes", "restaurants"
   add_foreign_key "restaurants", "users"
   add_foreign_key "tab_dishes", "dishes"
