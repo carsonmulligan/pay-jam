0c402ef0afede9e4f2dc6250a17fca31ef1701a2
carson mulligan <carsonmulligan45@gmail.com>
2022-06-01 12:20:01 +0100

    bill controller added lol

---
+++ app/controllers/bills_controller.rb
@@ -0,0 +1,13 @@
+class BillsController < ApplicationController
+  def index
+    @bills = Bill.all
+  end
+
+  def show
+    @bill = Bill.find(params[:id])
+  end
+
+#   def create
+#     @bill = Bill.new()
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'bills/index'
+  get 'bills/show'
+  get 'bills/create'
   devise_for :users
   root to: 'pages#home'
 
