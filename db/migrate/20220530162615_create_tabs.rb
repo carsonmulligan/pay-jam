483bcce2998951523f3a744c50a05c24fad1963b
carson mulligan <carsonmulligan45@gmail.com>
2022-06-02 16:11:42 +0100

    migration:

--- db/migrate/20220530162615_create_tabs.rb
+++ db/migrate/20220530162615_create_tabs.rb
@@ -2,7 +2,7 @@ class CreateTabs < ActiveRecord::Migration[6.1]
   def change
     create_table :tabs do |t|
       t.string :hash
-      t.integer :total
+      t.monetize :total, currency: { present: false}
       t.integer :status
       t.integer :table_number
       t.references :restaurant, null: false, foreign_key: true
