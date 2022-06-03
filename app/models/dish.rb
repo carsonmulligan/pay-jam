fb99360c2197a137ec9f3c9e529082e9d8c866e7
carson mulligan <carsonmulligan45@gmail.com>
2022-06-03 11:47:19 +0100

    complain stopped

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,7 +1,7 @@
 class Dish < ApplicationRecord
   belongs_to :restaurant
 
-  has_many :tab_dishes
+  has_many :tab_dishes, dependent: :destroy
   has_many :tabs, through: :tab_dishes
   has_one_attached :photo
 end
