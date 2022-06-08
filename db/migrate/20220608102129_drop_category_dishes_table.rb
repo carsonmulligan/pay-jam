a8372534a6a87d6806e10753dceb215463ac6d2a
andras nemeth <andras.nemeth02@gmail.com>
2022-06-08 12:44:28 +0100

    Add categories model, fix bug in dishes index

--- db/migrate/20220608102129_drop_category_dishes_table.rb
+++
@@ -1,5 +0,0 @@
-class DropCategoryDishesTable < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :categories_dishes
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_102129) do
+ActiveRecord::Schema.define(version: 2022_06_08_102440) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,10 +71,11 @@ ActiveRecord::Schema.define(version: 2022_06_08_102129) do
     t.string "name"
     t.text "description"
     t.integer "price_cents", default: 0, null: false
-    t.string "category"
     t.bigint "restaurant_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id", null: false
+    t.index ["category_id"], name: "index_dishes_on_category_id"
     t.index ["restaurant_id"], name: "index_dishes_on_restaurant_id"
   end
 
